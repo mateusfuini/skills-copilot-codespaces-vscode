7a5dd27f23934459452efe2bee907c7f62401d40
MATEUS GUILHERME FUINI <59875329+mateusfuini@users.noreply.github.com>
2023-07-18 13:13:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,70 @@
+// Create web server using copilot suggestions
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: skills-copilot-codespaces-vscode/express.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: skills-copilot-codespaces-vscode/express.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: skills-copilot-codespaces-vscode/express.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: skills-copilot-codespaces-vscode/express.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// Path: skills-copilot-codespaces-vscode/express.js
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a route
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// Start the web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!
